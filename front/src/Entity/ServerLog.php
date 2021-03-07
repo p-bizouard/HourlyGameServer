3eea7175de6f90e6f84e4d7bf8dc4a6db314061a
Paul Bizouard <bizouap@gmail.com>
2021-03-07 20:00:37 +0100

    Change log to longtext

--- front/src/Entity/ServerLog.php
+++ front/src/Entity/ServerLog.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace App\Entity;
 
 use App\Repository\ServerLogRepository;
@@ -37,7 +47,7 @@ class ServerLog
     private string $type;
 
     /**
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="text")
      * @Serializer\Groups({"list"})
      */
     private string $message;
